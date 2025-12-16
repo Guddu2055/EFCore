84fa01f5ff0a763472406cab793576ab090d44c9
guddu <guddu@HUAWEI>
2025-12-16 16:34:20 +0545

    dbcontext class added

---
+++ EFCoreOperation/EFCoreOperation/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreOperation.Data
+{
+    public class AppDbContext:DbContext
+    {
+        // appdbcontext pass their own method to Dbcontext like connection or etc.
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
